86a828c52176eed195e020b1415cfb95712ca4b8
Cihan <cihanbalkr@gmail.com>
2025-08-18 22:22:07 +0300

    Player adjustments & PlayerMovement script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using Unity.Netcode;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    private PlayerInputActions _playerInput;
+    [SerializeField] private float _speed;
+    private Camera _playerCamera;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if (IsOwner)
+        {
+            _playerCamera = Camera.main;
+            _playerCamera.transform.SetParent(transform, true);
+        }
+    }
+
+    private void Awake()
+    {
+        _playerInput = new PlayerInputActions();
+    }
+
+    
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    
+
+    private void Update()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        Vector2 moveInput = _playerInput.Player.Move.ReadValue<Vector2>();
+        Vector2 lookInput = _playerInput.Player.Look.ReadValue<Vector2>();
+
+        Debug.Log($"Move input X:{moveInput.x}, Y:{moveInput.y}");
+        Debug.Log($"Look input X:{lookInput.x}, Y:{lookInput.y}");
+
+        transform.Translate(new Vector3 (moveInput.x, 0,moveInput.y) * _speed * Time.deltaTime);
+        transform.rotation *= Quaternion.Euler(0, lookInput.x, 0);
+    }
+}
