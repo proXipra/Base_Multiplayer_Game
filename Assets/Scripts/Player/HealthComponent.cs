698c704a6a82bba8b8f42a6224e0bc0793593102
Cihan <cihanbalkr@gmail.com>
2025-08-24 22:23:36 +0300

    HealthComponent & implementation of its functionality

---
+++ Assets/Scripts/Player/HealthComponent.cs
@@ -0,0 +1,36 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class HealthComponent : NetworkBehaviour
+{
+    public NetworkVariable<float> health = new(100, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    [ServerRpc (RequireOwnership = false)]
+    public void ApplyDamageServerRpc(float damage, ulong attackerId)
+    {
+        health.Value -= damage;
+        LogDamageClientRpc(damage);
+
+        if (health.Value <= 0)
+        {
+            OnDeathClientRpc(attackerId, OwnerClientId);
+            return;
+        }
+    }
+
+    [ClientRpc]
+    void LogDamageClientRpc(float damage)
+    {
+        Debug.Log($"Player: {OwnerClientId} took, {damage} damage  .");
+    }
+
+    
+
+    [ClientRpc]
+    void OnDeathClientRpc(ulong attackerId, ulong ownerId)
+    {
+        Debug.Log($"Player: {ownerId} is killed by player {attackerId} .");
+    }
+
+
+}
--- Assets/Scripts/Player/WeaponController.cs
+++ Assets/Scripts/Player/WeaponController.cs
@@ -10,6 +10,7 @@ public class WeaponController : NetworkBehaviour
     private InputAction _attackAction;
 
     [SerializeField] private LayerMask hitMask;
+    [SerializeField] private float _weaponDamage = 25f;
     private const float RAY_OFFSET = 1.5f;
 
     private void Awake()
@@ -23,7 +24,7 @@ public class WeaponController : NetworkBehaviour
     public override void OnNetworkSpawn()
     {
         if (!IsOwner) return;
-
+        
         _playerInput.Enable();
         _attackAction.performed += AttackPerformed;
     }
@@ -49,21 +50,36 @@ public class WeaponController : NetworkBehaviour
     void AttackServerRpc()
     {
         RaycastHit hitInfo;
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y + RAY_OFFSET, transform.position.z);
+        //Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
 
-        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + RAY_OFFSET, transform.position.z), forward, Color.red, 20f);
+        Debug.DrawRay(origin, transform.forward * 10, Color.red, 20f);
 
-        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + RAY_OFFSET, transform.position.z), 
-            transform.forward, out hitInfo, 10f, hitMask))
+        if (Physics.Raycast(origin, transform.forward, out hitInfo, 10f, hitMask))
         {
-            HitClientRpc();
+            if (hitInfo.collider.GetComponentInParent<HealthComponent>() is HealthComponent healthComp)
+            {
+                healthComp.ApplyDamageServerRpc(_weaponDamage, OwnerClientId);
+            }
+        
+            else
+            {
+                LogClientRpc();
+            }
         }
     }
 
-
     [ClientRpc]
-    private void HitClientRpc()
+    void LogClientRpc()
     {
-        Debug.LogWarning("Hit occured |ClientRPC|");
+        Debug.Log("Hit happend but lack of component");
     }
+
+    //[ClientRpc]
+    //private void HitClientRpc(Transform transform)
+    //{
+    //    Debug.LogWarning("Hit");
+
+    //    transform.GetComponent<HealthComponent>().ApplyDamageServerRpc(_weaponDamage, OwnerClientId);
+    //}
 }
